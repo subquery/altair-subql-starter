5681ebb5d71249ac75e6e2c47fc0418db360fb7e
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-01-27 16:46:42 +1300

    Update chaintypes.ts

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -3,30 +3,30 @@ import type { OverrideBundleDefinition } from '@polkadot/types/types';
 /* eslint-disable sort-keys */
 
 const definitions: OverrideBundleDefinition = {
-    types: [
-        {
-            // on all versions
-            minmax: [0, undefined],
-            types: {
-                ParachainAccountIdOf: 'AccountId',
-                Proof: {
-                    leafHash: 'Hash',
-                    sortedHashes: 'Vec<Hash>'
-                },
-                ProxyType: {
-                    _enum: [
-                        'Any',
-                        'NonTransfer',
-                        'Governance',
-                        '_Staking',
-                        'NonProxy'
-                    ]
-                },
-                RelayChainAccountId: 'AccountId',
-                RootHashOf: 'Hash'
-            }
-        }
-    ]
+  types: [
+    {
+      // on all versions
+      minmax: [0, undefined],
+      types: {
+        ParachainAccountIdOf: 'AccountId',
+        Proof: {
+          leafHash: 'Hash',
+          sortedHashes: 'Vec<Hash>'
+        },
+        ProxyType: {
+          _enum: [
+            'Any',
+            'NonTransfer',
+            'Governance',
+            '_Staking',
+            'NonProxy'
+          ]
+        },
+        RelayChainAccountId: 'AccountId',
+        RootHashOf: 'Hash'
+      }
+    }
+  ]
 };
 
-export default { typesBundle: definitions };
+export default { typesBundle: { spec: { altair: definitions }}};
